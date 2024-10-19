3e90b6e6a23872efe846bb7b110f21f85d345287
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-10-19 12:18:50 -0300

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quiz-for-kids",
-    version="0.1.2",
+    version="0.1.3",
     author="Fernando Pujaico Rivera",
     author_email="fernando.pujaico.rivera@gmail.com",
     description="A fun quiz library for kids",
